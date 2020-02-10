038d25434516d80575b71827e945b8a17eefe9e7
LioKlio <liona.tomm@gmail.com>
2020-02-10 03:01:43 +0200

    some layout fix; chart

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,3 +6,16 @@ window.onload = () => {
     })
   })
 }
+
+let pointData = [1, 1.45, 1.83, 2.27, 3.6, 4.1, 7.8, 15.6];
+let riseChart = document.querySelector('.rise-chart');
+let risePoints = document.querySelectorAll('.rise-point');
+let pointW = (riseChart.offsetWidth - 160) / risePoints.length;
+
+risePoints.forEach((point, inx) => {
+  point.style.width = pointW + 'px';
+  point.style.left = 80 + (inx ?  pointW*inx : 0) + 'px';
+  point.style.top = (inx ?  100-(pointData[inx]*riseChart.offsetHeight/100) : 100) + '%';
+  point.querySelector('.btn').textContent = pointData[inx] + '%'
+
+})
